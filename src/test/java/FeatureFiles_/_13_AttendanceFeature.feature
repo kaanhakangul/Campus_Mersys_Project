188c4dd843ee69741152d73dee63e3d8e2959b41
ArcCodes <cinucevasli@gmail.com>
2024-06-08 16:56:36 +0300

    US13 tamamlandi

---
+++ src/test/java/FeatureFiles_/_13_AttendanceFeature.feature
@@ -0,0 +1,8 @@
+Feature: Hamburger Menu
+
+  Background:
+    Given Navigate to Campus
+    When Enter valid username and password and click login button
+
+  Scenario:Report The Excuses
+    Then Click on the Attendance Excuses
--- src/test/java/Pages/ParentPage.java
+++ src/test/java/Pages/ParentPage.java
@@ -1,14 +1,19 @@
 package Pages;
 
 import Utilities.GWD;
+import Utilities.Tools;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 
 public class ParentPage {
@@ -41,5 +46,36 @@ public class ParentPage {
         new Actions(GWD.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
         //Sayfaya esc tuşu gönderildi.
     }
+
+    public void myUploadFile(String pathFile) {
+
+        Robot robot;
+
+        {
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        StringSelection createPathFile = new StringSelection(pathFile);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(createPathFile, null);
+
+        Tools.Wait(1);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+
+        Tools.Wait(1);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        Tools.Wait(1);
+
+    }
+
+
 
 }
